2afd91b9b8d05b0ad65983140e64a25d1ce8a2e9
waychean10 <waychean10@gmail.com>
2023-09-02 19:23:13 -0500

    Remove background image svg, move header under main

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,4 @@
 import React from 'react';
-import { Header } from './components/Header';
 import { MainBody } from './components/MainBody';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './App.css';
@@ -7,7 +6,6 @@ import './App.css';
 const App: React.FC = () => {
   return (
       <Router>
-        <Header />
         <MainBody />
       </Router>
   );
--- src/components/MainBody.tsx
+++ src/components/MainBody.tsx
@@ -1,12 +1,13 @@
 import { useState } from 'react';
 import { BrowserRouter as Router, Route, Link, Routes } from 'react-router-dom';
+import { Header } from './Header';
 import backgroundImage from  '../assets/test-background.jpg';
 
 const MainBody: React.FC = () => {
 
     return (
-        <main style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }} className='w-96 h-96 bg-no-repeat'>
-
+        <main style={{ backgroundImage: `url(${backgroundImage})`, backgroundSize: 'cover' }} className='w-full h-screen bg-no-repeat'>
+            <Header />
         </main>
     )
 }
